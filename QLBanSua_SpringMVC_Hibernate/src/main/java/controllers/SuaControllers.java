7cd35d115ee0a62e1a0155d709f0d9661a0e5db0
Võ Lê Phúc <kikyo0310@gmail.com>
2021-10-28 18:54:46 +0700

    hien thi trang sua

--- QLBanSua_SpringMVC_Hibernate/src/main/java/controllers/SuaControllers.java
+++ QLBanSua_SpringMVC_Hibernate/src/main/java/controllers/SuaControllers.java
@@ -1,5 +1,16 @@
 package controllers;
 
-public class SuaControllers {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import bussinesLogic.SuaBL;
+
+@Controller
+public class SuaControllers {
+	@RequestMapping(path = "/TrangSanPhamSua.html")
+	public String trangSua(Model model) {
+		model.addAttribute("dss", SuaBL.docTatCa(0, 8));
+		return "trangsua";
+	}
 }
